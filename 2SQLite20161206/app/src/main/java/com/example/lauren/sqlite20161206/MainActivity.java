ac53d18f74d18cd12652bd7ac3da3931ad0bb90d
lilongWork <lilongcnc@vip.qq.com>
2016-12-08 12:33:10 +0800

    增加 代码数据库事务代码

--- 2SQLite20161206/app/src/main/java/com/example/lauren/sqlite20161206/MainActivity.java
+++ 2SQLite20161206/app/src/main/java/com/example/lauren/sqlite20161206/MainActivity.java
@@ -81,6 +81,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             default:
                 break;
         }
-
     }
+//        //1.创建一个帮助类的对象
+//        BankOpenHelper bankOpenHelper = new BankOpenHelper(this);
+//        //2.调用数据库帮助类对象的getReadableDatabase创建数据库，初始化表数据，获取一个SqliteDatabase对象去做转账（sql语句）
+//        SQLiteDatabase db = bankOpenHelper.getReadableDatabase();
+//        //3.转账,将李四的钱减200，张三加200
+//        db.beginTransaction();//开启一个数据库事务
+//        try {
+//            db.execSQL("update account set money= money-200 where name=?",new String[]{"李四"});
+//            int i = 100/0;//模拟一个异常
+//            db.execSQL("update account set money= money+200 where name=?",new String[]{"张三"});
+//
+//            db.setTransactionSuccessful();//标记事务中的sql语句全部成功执行
+//        } finally {
+//            db.endTransaction();//判断事务的标记是否成功，如果不成功，回滚错误之前执行的sql语句
+//        }
+
+
+
 }
