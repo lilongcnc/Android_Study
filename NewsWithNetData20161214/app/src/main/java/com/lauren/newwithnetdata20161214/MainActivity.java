489983a315e958751da61e4e1d458dbfa3ca89e2
lilongWork <lilongcnc@vip.qq.com>
2016-12-22 16:57:57 +0800

    修正 NewListView 获取服务器数据和数据库数据逻辑

--- NewsWithNetData20161214/app/src/main/java/com/lauren/newwithnetdata20161214/MainActivity.java
+++ NewsWithNetData20161214/app/src/main/java/com/lauren/newwithnetdata20161214/MainActivity.java
@@ -1,28 +1,19 @@
 package com.lauren.newwithnetdata20161214;
 
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.lauren.newwithnetdata20161214.adapter.NewsListViewAdapter;
-import com.lauren.newwithnetdata20161214.dao.InfoDaoUtils;
-import com.lauren.newwithnetdata20161214.model.NewsGetNetDataUtil;
+import com.lauren.newwithnetdata20161214.model.NewsUtils;
 import com.lauren.newwithnetdata20161214.model.RecommondInfo;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
@@ -30,43 +21,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     private Context mContext;
     private ListView lsit_listView;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mContext = this;
-
-
-        //创建listView
-        lsit_listView = (ListView) findViewById(R.id.lsit_listView);
-
-
-        //先从数据库中读取数据
-        ArrayList<RecommondInfo> dbArrayList = new InfoDaoUtils(mContext).query();
-        if (dbArrayList != null && dbArrayList.size()>0)
-        {
-            lsit_listView .setAdapter(new NewsListViewAdapter(mContext,dbArrayList));
-        }
-
-
-        //请求网络数据
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-
-                ArrayList<RecommondInfo> recommondList  = NewsGetNetDataUtil.getAllNewsForDatabase(mContext);
-
-                //传递数据主线程更新
-                Message message = new Message();
-                message.obj = recommondList;
-                handler.sendMessage(message);
-            }
-        }).start(); //记住要启动线程啊
-
-        //设置listview条目的点击事件
-        lsit_listView.setOnItemClickListener(this);
-    }
 
 
 
@@ -80,6 +34,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             if (recommondList.size() > 0 && recommondList != null)
             {
                 //设置listView显示
+                Log.e("主线程中更新网络请求 ", recommondList.size()+"" );
                 lsit_listView.setAdapter(new NewsListViewAdapter(mContext, recommondList));
             }
         }
@@ -87,12 +42,62 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
 
     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mContext = this;
+
+
+        //创建listView
+        lsit_listView = (ListView) findViewById(R.id.lsit_listView);
+
+
+        //先从数据库中读取数据
+        ArrayList<RecommondInfo> dbArrayList = NewsUtils.getAllNewsForDatabase(mContext);
+
+        if (dbArrayList != null && dbArrayList.size()>0)
+        {
+            Log.e("第一次进入APP从数据库中获取到数据 ", dbArrayList.size()+"" );
+
+            NewsListViewAdapter newsAdapter = new NewsListViewAdapter(mContext,dbArrayList);
+            lsit_listView.setAdapter(newsAdapter);
+
+//            lsit_listView .setAdapter(new NewsListViewAdapter(mContext,dbArrayList));
+        }
+
+
+        //请求网络数据
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                ArrayList<RecommondInfo> recommondList  = NewsUtils.getAllNewsForNetWork(mContext);
+
+                System.out.print("---------------------------------------------------");
+                Log.e("run ", recommondList.size()+"" );
+                //传递数据主线程更新
+                Message message = new Message();
+                message.obj = recommondList;
+                handler.sendMessage(message);
+            }
+        }).start(); //记住要启动线程啊
+
+
+
+        //设置listview条目的点击事件
+        lsit_listView.setOnItemClickListener(this);
+    }
+
+
+
+
+    @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
-        Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse("http://www.lilongcnc.cc"));
-        startActivity(intent);
+//        Intent intent = new Intent();
+//        intent.setAction(Intent.ACTION_VIEW);
+//        intent.setData(Uri.parse("http://www.lilongcnc.cc"));
+//        startActivity(intent);
 
     }
 }
--- NewsWithNetData20161214/app/src/main/java/com/lauren/newwithnetdata20161214/dao/MySQliteOpenHelper.java
+++ NewsWithNetData20161214/app/src/main/java/com/lauren/newwithnetdata20161214/dao/MySQliteOpenHelper.java
@@ -3,6 +3,7 @@ package com.lauren.newwithnetdata20161214.dao;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by Lauren on 16/12/6.
@@ -20,20 +21,33 @@ public class MySQliteOpenHelper  extends SQLiteOpenHelper{
     //oncreate方法是数据库第一次创建的时候会被调用;  特别适合做表结构的初始化,需要执行sql语句；SQLiteDatabase db可以用来执行sql语句
     @Override
     public void onCreate(SQLiteDatabase db) {
+        Log.e("xxxxxxxx","xxxxxxxxxxxx");
+        System.out.println(db);
+
+//        db.execSQL("create table news (_id integer  ,title varchar(200),des varchar(300),icon_url varchar(200),news_url varchar(200)," +
+//                " type integer , time varchar(100),comment integer)");
+
         //通过SQLiteDatabase执行一个创建表的sql语句
-        db.execSQL("create table info (_id integer primary key autoincrement," +
+//        db.execSQL("create table info (_id integer," +
+//                "logo varchar(200)," +
+//                "CustomerName varchar(200)," +
+//                "Intro varchar(200)," +
+//                "TypeName varchar(200)," +
+//                "ReturnCredit varchar(200)," +
+//                "SoloCreditPercent varchar(200))");
+
+
+        db.execSQL("create table info (_id integer," +
                 "logo text not null," +
                 "CustomerName text not null," +
                 "Intro text not null," +
                 "TypeName text not null," +
                 "ReturnCredit text not null," +
-                "SoloCreditPercent text not null");
+                "SoloCreditPercent text not null)");
     }
 
     //onUpgrade数据库版本号发生改变时才会执行； 特别适合做表结构的修改
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        //添加一个phone字段
-//		db.execSQL("alter table info add phone varchar(11)");
     }
 }
